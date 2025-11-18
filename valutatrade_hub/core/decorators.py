f82755b866c0550e0a4dcc7c0a44a9120ede4c96
arthuras <arthuras.work@gmail.com>
2025-11-19 01:21:38 +0300

    final-check

--- valutatrade_hub/core/decorators.py
+++ valutatrade_hub/core/decorators.py
@@ -36,7 +36,7 @@ def handler_api_errors(func):
             log.info('Starting rates update...')
             return func(*args, **kwargs) 
         except Exception as e:
-            if e == 'conversion_rates':
+            if str(e) == "'conversion_rates'":
                 log.alert('Set env var EXCHENGE_API_KEY')
                 return None
             log.alert(f'Error in {func.__name__}: {e}')
